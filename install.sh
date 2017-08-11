1c93d0ae0a294696c679ee8aaedf2a60555d661e
Jiren Jin <jinjiren@foxmail.com>
2017-08-11 16:30:29 +0900

    fix bugs and add dircolors for solarized colorscheme

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # zsh, vim, tmux with relatively new version
 
 # clone the dot-files repo first
-git clone git://github.com/jinjiren/dot-files.git ~/dot-files
+git clone --recursive git://github.com/jinjiren/dot-files.git ~/dot-files
 
 # soft link for dot files
 ln -s ~/dot-files/zsh-config/zshrc ~/.zshrc
@@ -13,9 +13,13 @@ ln -s ~/dot-files/vim-config ~/.vim
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # change default shell to zsh
 chsh -s /bin/zsh
-source ~/.zshrc
 # zsh plugins
+ZSH_CUSTOM=$HOME/dot-files/zsh-config/custom
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh themes
 git clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
+# dircolors for solarized scheme
+git clone https://github.com/seebi/dircolors-solarized
+cp dircolors-solarized/dircolors.256dark ~/.dircolors
+rm -rf dircolors-solarized
