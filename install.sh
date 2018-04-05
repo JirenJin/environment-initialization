90883e064c3209e93ed98a4ca47e9e02ebe7dbba
Jiren Jin <jinjiren@foxmail.com>
2018-04-05 16:17:42 +0900

    update comments, and be compatible with the new dot-files repo

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 # zsh, vim, tmux with relatively new version
 
 # clone the dot-files repo first
-git clone --recursive git@github.com:jinjiren/dot-files.git ~/dot-files
+# note that you need to use the `https` url if you are not `jinjiren` :)
+git clone git@github.com:jinjiren/dot-files.git ~/dot-files
 
 # soft link for dot files
 ln -s ~/dot-files/zsh-config/zshrc ~/.zshrc
@@ -12,28 +13,36 @@ ln -s ~/dot-files/vim-config ~/.vim
 # install zsh, oh-my-zsh (git is required)
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # change default shell to zsh
+# maybe you need to do this manually
 chsh -s /bin/zsh
 # zsh plugins
 ZSH_CUSTOM=$HOME/dot-files/zsh-config/custom
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh themes
+# you may need to manually customize the font for your terminal (e.g., GNOME or XTerm2)
 git clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
 # dircolors for solarized scheme
 git clone https://github.com/seebi/dircolors-solarized
 cp dircolors-solarized/dircolors.256dark ~/.dircolors
 rm -rf dircolors-solarized
 
-# install pyenv
+# install pyenv (optional)
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-source ~/.zshrc
-pyenv install anaconda3-5.0.1
-pyenv rehash
-pyenv gloabl anaconda3-5.0.1
+# source ~/.zshrc
+
+# intall the python version you need
+#pyenv install anaconda3-5.0.1
+#pyenv rehash
+#pyenv gloabl anaconda3-5.0.1
 
 # install cudnn
 pip install cudnnenv
-cudnnenv install v7-cuda8
+# choose your own version of cuda and cudnn
+# cudnnenv install v7-cuda8
 
 # install vim plugins
+# note that it's better to install your python properly first before running this
+# because building YouCompleteMe (vim plugin for auto-completion) requires some dependencies of Python
+# as well as cmake, etc.
 vim +PlugInstall +qall!
